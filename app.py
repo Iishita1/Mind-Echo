bb17c2f67b3be9cb6cc7573f789dba421ccddd04
Ishita <nope.ishita@gmail.com>
2025-03-26 10:41:54 +0530

    many

--- app.py
+++ app.py
@@ -6,9 +6,21 @@ app = Flask(__name__)
 def landing():
     return render_template('landing.html')
 
+@app.route('/landing01')
+def landing01():
+    return render_template('landing01.html')
+
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/exercise')
+def exercise():
+    return render_template('exercise.html')
 
 @app.route('/settings')
 def settings():
@@ -18,17 +30,9 @@ def settings():
 def quiz():
     return render_template('quiz.html')
 
-@app.route('/exercise')
-def exercise():
-    return render_template('exercise.html')
-
 @app.route('/chatbot')
 def chatbot():
     return render_template('chatbot.html')
-
-@app.route('/landing01')
-def landing01():
-    return render_template('landing01.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
